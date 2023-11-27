9ba3938e3ae5af0d2931aea366281d82590d8076
neradleo <neradleo@gmail.com>
2023-11-27 18:44:24 +0800

    Skipping copy-list, starting Card Form segment

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
@@ -29,7 +29,7 @@ export const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
   const { execute: executeDelete } = useAction(deleteList, {
     onSuccess: (data) => {
       toast.success(`List "${data.title}" deleted`);
-      //   closeRef.current?.click();
+      closeRef.current?.click();
     },
     onError: (error) => {
       toast.error(error);
@@ -55,7 +55,6 @@ export const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
         <div className="text-sm font-medium text-center text-neutral-600 pb-4">
           List actions
         </div>
-        {/* <PopoverClose asChild> */}
         <PopoverClose ref={closeRef} asChild>
           <Button
             className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
