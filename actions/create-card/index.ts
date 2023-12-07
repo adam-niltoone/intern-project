40a2cf90e7acd0837c140e76d3bfc08f6e696f16
neradleo <neradleo@gmail.com>
2023-12-07 15:48:56 +0800

    quick save

--- actions/create-card/index.ts
+++ actions/create-card/index.ts
@@ -6,7 +6,7 @@ import { revalidatePath } from "next/cache";
 import { db } from "@/lib/db";
 import { createSafeAction } from "@/lib/create-safe-action";
 
-import { CreateList } from "./schema";
+import { CreateCard } from "./schema";
 import { InputType, ReturnType } from "./types";
 // import { createAuditLog } from "@/lib/create-audit-log";
 // import { ACTION, ENTITY_TYPE } from "@prisma/client";
@@ -20,53 +20,48 @@ const handler = async (data: InputType): Promise<ReturnType> => {
     };
   }
 
-  const { title, boardId } = data;
-  let list;
+  const { title, boardId, listId } = data;
+  let card;
 
   try {
-    const board = await db.board.findUnique({
+    const list = await db.list.findUnique({
       where: {
-        id: boardId,
-        orgId,
+        id: listId,
+        board: {
+          orgId,
+        },
       },
     });
 
-    if (!board) {
+    if (!list) {
       return {
-        error: "Board not found",
+        error: "List not found",
       };
     }
 
-    const lastList = await db.list.findFirst({
-      where: { boardId: boardId },
+    const lastCard = await db.card.findFirst({
+      where: { listId },
       orderBy: { order: "desc" },
       select: { order: true },
     });
 
-    const newOrder = lastList ? lastList.order + 1 : 1;
+    const newOrder = lastCard ? lastCard.order + 1 : 1;
 
-    list = await db.list.create({
+    card = await db.card.create({
       data: {
         title,
-        boardId,
+        listId,
         order: newOrder,
       },
     });
-
-    // await createAuditLog({
-    //   entityTitle: list.title,
-    //   entityId: list.id,
-    //   entityType: ENTITY_TYPE.LIST,
-    //   action: ACTION.CREATE,
-    // })
   } catch (error) {
     return {
-      error: "Failed to create."
-    }
+      error: "Failed to create.",
+    };
   }
 
   revalidatePath(`/board/${boardId}`);
-  return { data: list };
+  return { data: card };
 };
 
-export const createList = createSafeAction(CreateList, handler);
+export const createCard = createSafeAction(CreateCard, handler);
