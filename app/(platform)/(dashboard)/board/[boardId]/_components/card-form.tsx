0acf4c0bddfa22e09fd358fba1e7c99659754064
neradleo <neradleo@gmail.com>
2023-12-07 15:35:30 +0800

    Add card feature in progress

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { forwardRef } from "react";
+import { Plus, X } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { FormSubmit } from "@/components/form/form-submit";
+import { FormTextarea } from "@/components/form/form-textarea";
+
+interface CardFormProps {
+  listId: string;
+  enableEditing: () => void;
+  disableEditing: () => void;
+  isEditing: boolean;
+}
+
+export const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(
+  ({ listId, enableEditing, disableEditing, isEditing }, ref) => {
+    if (isEditing) {
+      return (
+        <form
+          // ref={formRef}
+          // action={onSubmit}
+          className="m-1 py-0.5 px-1 space-y-4"
+        >
+          <FormTextarea
+            id="title"
+            onKeyDown={() => {}}
+            // onKeyDown={onTextareakeyDown}
+            ref={ref}
+            placeholder="Enter a title for this card..."
+            // errors={fieldErrors}
+          />
+          <input hidden id="listId" name="listId" value={listId} />
+          <div className="flex items-center gap-x-1">
+            <FormSubmit>Add card</FormSubmit>
+            <Button onClick={disableEditing} size="sm" variant="ghost">
+              <X className="h-5 w-5" />
+            </Button>
+          </div>
+        </form>
+      );
+    }
+
+    return (
+      <div className="pt-2 px-2">
+        <Button
+          onClick={enableEditing}
+          className="h-auto px-2 py-1.5 w-full justify-start text-muted-foreground text-sm"
+          size="sm"
+          variant="ghost"
+        >
+          <Plus className="h-4 w-4 mr-2" />
+          Add a card
+        </Button>
+      </div>
+    );
+  }
+);
+
+CardForm.displayName = "CardForm";
