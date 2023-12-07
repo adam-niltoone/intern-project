73235925b02ab0207d35b002ae76fd27007d1739
neradleo <neradleo@gmail.com>
2023-12-07 15:34:14 +0800

    Add card feature in progress

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-items.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-items.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import { ElementRef, useRef, useState } from "react";
+
 import { ListWithCards } from "@/types";
 
+import { CardForm } from "./card-form";
 import { ListHeader } from "./list-header";
 
 interface ListItemProps {
@@ -10,10 +13,33 @@ interface ListItemProps {
 }
 
 export const ListItem = ({ data, index }: ListItemProps) => {
+  const textareaRef = useRef<ElementRef<"textarea">>(null);
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      textareaRef.current?.focus();
+    });
+  };
+
   return (
-    <li className="shrink-0 h-full w-[200px] select-none">
+    <li className="shrink-0 h-full w-[272px] select-none">
+      {/* <li className="shrink-0 h-full w-[200px] select-none"> */}
       <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2 ">
-        <ListHeader data={data} />
+        <ListHeader onAddCard={enableEditing} data={data} />
+        <CardForm
+          listId={data.id}
+          ref={textareaRef}
+          isEditing={isEditing}
+          enableEditing={enableEditing}
+          disableEditing={disableEditing}
+        />
       </div>
     </li>
   );
